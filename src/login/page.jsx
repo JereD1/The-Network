6175f36b231fb1781aa98a6c0f2a696751c1e23b
MereaD <jereadgaming2@gmail.com>
2024-09-22 22:14:09 +0100

    updated repo

---
+++ src/login/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div className='flex'>
+        <div className='flex-1 p-20'>
+        {/* left column content */}
+        </div>
+        <div className='flex-1 p-20'>
+            {/* Right column content */}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default page
