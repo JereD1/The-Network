d610b181b0065fd4c0a027c51102325d2f424877
MereaD <jereadgaming2@gmail.com>
2024-09-22 22:45:29 +0100

    vite update

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -2,10 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import {getAuth} from 'firebase/storage'
 
-<<<<<<< HEAD
-
-=======
->>>>>>> master
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
