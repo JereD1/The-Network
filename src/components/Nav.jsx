1e55435d33091a612827ca0267c3a59bd94bd1ba
JereD1 <omoruyijeremiah3@gmail.com>
2024-09-24 12:36:31 +0100

    Cleaned files

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,10 +6,13 @@ const Nav = () => {
     <div className='flex flex-row items-center justify-center '>
         <div className=''>
       <div className='flex items-center gap-56'>
-        <img src={MainNetworkLogo} alt="The Network Logo" className='w-40'/>        
+        <img src={MainNetworkLogo} alt="The Network Logo" className='invert w-40'/> 
+        <div>
+          
+          </div>       
         <div className='flex gap-10'>
-            <button className='text-white border-green-500 border rounded-full px-6 py-2 hover:bg-green-600'>Sign Up</button>
-            <button className='text-white bg-green-500 px-6 py-2 rounded-full hover:bg-green-600'>Login</button>
+            <button className='text-white border-green-500 border rounded-lg px-6 py-2 hover:bg-green-600'>Sign Up</button>
+            <button className='text-white bg-green-500 px-6 py-2 rounded-lg hover:bg-green-600'>Login</button>
             </div>
       </div>
     </div>
