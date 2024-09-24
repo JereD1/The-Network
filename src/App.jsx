0cea11670ef3963513dde07699e2ae9010b58ade
JereD1 <omoruyijeremiah3@gmail.com>
2024-09-24 10:42:38 +0100

    Path Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React from 'react'
-import Nav from '../../components/Nav'
-import Footer from '../../components/Footer'
-import CreateTeam from '../../components/CreateTeam'
-import MissionVission from '../../components/MissionVission'
-import AboutUs from '../../components/AboutUs'
-import BuiltDiff from '../../components/BuiltDifferent'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import CreateTeam from './components/CreateTeam'
+import MissionVission from './components/MissionVission'
+import AboutUs from './components/AboutUs'
+import BuiltDiff from './components/BuiltDifferent'
 
 
 const App = () => {
